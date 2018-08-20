6f7ccbbbcdd1daa16f4a6d22115fed16c045a033
Nick Dambrosio <ndambro303@gmail.com>
2018-08-20 16:56:53 -0600

    Add tail draw method

--- Tail.js
+++
@@ -1,9 +0,0 @@
-const GamePiece = require('./GamePiece');
-
-class Tail extends GamePiece {
-  constructor(x, y, height, width, color) {
-    super(x, y, height, width, color);
-  }
-}
-
-module.exports = Tail;
