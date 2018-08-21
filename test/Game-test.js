64d115ba95d572d4ecd0e91aaf49e6cfa2e1d6f7
Nick Dambrosio <ndambro303@gmail.com>
2018-08-21 15:37:13 -0600

    start testing

--- test/Game-test.js
+++ test/Game-test.js
@@ -7,16 +7,18 @@ const ctx = {
     width: 600,
     height: 600
   }
-}
+};
 
 describe('Game', () => {
   it('should end the game if block collides with wall', () =>  {
 
     const game = new Game(ctx);
-    game.animate()
-  })
-  it('should take properties', () => {})
-  it('should collide with walls', () => {})
-  it('should be able to move', () => {})
-  it('should be able to changeDirection', () => {})
-})
+
+    game.animate();
+  });
+
+  it('should take properties', () => {});
+  it('should collide with walls', () => {});
+  it('should be able to move', () => {});
+  it('should be able to changeDirection', () => {});
+});
