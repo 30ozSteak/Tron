a246c04ae58c385a455879244a0d4d2b99e22cd4
Nick Dambrosio <ndambro303@gmail.com>
2018-08-22 01:36:16 -0600

    Game-piece tests

--- test/GamePiece-test.js
+++ test/GamePiece-test.js
@@ -1,50 +1,76 @@
 const { assert } = require('chai');
 const GamePiece = require('../lib/GamePiece.js');
+const Game = require('../lib/Game.js');
+const Tail = require('../lib/Tail.js');
+
+const ctx = {
+  canvas: {
+    width: 600,
+    height: 600
+  }
+};
 
 describe('GamePiece', () => {
   let gamepiece;
 
   beforeEach(() => {
-    gamepiece = new GamePiece(30, 30, 10, 10, 'green');
+    gamepiece = new GamePiece(30, 30, 10, 10, 'blue');
   });
 
   it('should take properties', () => {
-    // Assertion
     assert.deepEqual(gamepiece, {
       x: 30,
       y: 30,
       height: 10,
       width: 10,
-      color: 'green',
+      color: 'blue',
       dx: 1,
       dy: 0,
       dxv: 2,
       dyv: 2
     });
-    // Teardown
   });
 
   it('should collide with a second gamepiece that occupies the same space', () => {
-    const gamepiece2 = new GamePiece(30, 30, 10, 10, 'green');
+    const gamepiece2 = new GamePiece(30, 30, 10, 10, 'blue');
 
-    // Execution
     const colliding = gamepiece.isCollidingWith(gamepiece2);
 
-    // Assertion
     assert.isTrue(colliding);
   });
 
   it('should not collide with a second gamepiece that does not occupy the same space', () => {
-    const gamepiece2 = new GamePiece(130, 130, 10, 10, 'green');
+    const gamepiece2 = new GamePiece(130, 130, 10, 10, 'blue');
 
-    // Execution
     const colliding = gamepiece.isCollidingWith(gamepiece2);
 
-    // Assertion
     assert.isFalse(colliding);
   });
 
-  // it('should collide with walls', () => {})
-  // it('should be able to move', () => {})
-  // it('should be able to changeDirection', () => {})
+  it('should not be able to collide with wall', () =>  {
+    const gamepiece = new GamePiece(700, 700, 10, 10, 'blue');
+    const isColliding = gamepiece.isCollidingWithWall(700, 700);
+
+    assert.isTrue(isColliding);
+  });
+
+  it('should be able to move', () => {
+    gamepiece.move({dx: 1, dy: 1});
+
+    assert.equal(gamepiece.dx, 1)
+  });
+
+  it('should be able to change direction', () => {
+    gamepiece.move({dx: 0, dy: 0});
+    gamepiece.changeDirection({dx: 0, dy:1})
+    assert.equal(gamepiece.dy, 1);
+  })
+
+  it('shouldnt be able to drive backwards on itself', () => {
+    gamepiece.move({dx: 1, dy: 0});
+    gamepiece.changeDirection({dx: -1, dy: 0});
+    gamepiece.preventDrivingBackwards({dx: -1, dy: 0})
+    
+    assert.equal(gamepiece.dx, 1);
+  })
 });
